ea1bbb176ef4ad90d3eb1ff3a2ad34d93c8a4ea5
Glebson Santos <glebsonsantos@outlook.pt>
2024-07-18 01:34:32 -0300

    Feat: Add DbContext

---
+++ src/TaQuanto.Infra/Data/TaQuantoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaQuanto.Domain.Entities;
+
+namespace TaQuanto.Infra.Data
+{
+    public class TaQuantoContext : IdentityDbContext<User>
+    {
+        public TaQuantoContext(DbContextOptions<TaQuantoContext> options) : base(options)
+        {
+        }
+    }
+}
