b540aad13b35642e71a86d4a380d2b3d074d4e42
Glebson Santos <glebsonsantos@outlook.pt>
2024-07-26 15:11:44 -0300

    Feat: Add Configured File for JwtSettings and Extencion Class

---
+++ src/TaQuanto.Service/Helpers/IServiceCollectionExtencion.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaQuanto.Service.Interfaces;
+using TaQuanto.Service.Services;
+
+namespace TaQuanto.Service.Helpers
+{
+    public static class IServiceCollectionExtencion
+    {
+        public static void AddService(this IServiceCollection service, IConfiguration config)
+        {
+            AddClassConfigJwt(service, config);
+        }
+
+        public static void AddClassConfigJwt(IServiceCollection service, IConfiguration config) 
+        {
+            JwtConfig jwtConfig = new JwtConfig();
+            config.GetSection("JWT").Bind(jwtConfig);
+            service.AddSingleton(jwtConfig);
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+        }
+    }
+}
---
+++ src/TaQuanto.Service/Helpers/JwtConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace TaQuanto.Service.Helpers
+{
+    public class JwtConfig
+    {
+        public string ValidAudience { get; set; }
+        public string ValidIssuer { get; set; }
+        public string SecretKey { get; set; }
+        public int TokenValidityInMinutes { get; set; }
+        public int RefreshTokenValidityInMinutes { get; set; }
+    }
+}
